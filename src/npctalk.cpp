8c089868fad9da9a5e6d2d2c26e6d6ecf764d0bd
jo <josef.horvath@gmx.at>
2026-01-09 16:25:17 +0100

    Start

--- src/npctalk.cpp
+++ src/npctalk.cpp
@@ -1183,6 +1183,7 @@ void game::chat()
     std::string yell_msg;
     std::string emote_msg;
     bool is_order = true;
+    bool is_sentence_yell = false;
     nmenu.query();
 
     if( nmenu.ret < 0 ) {
@@ -1213,6 +1214,7 @@ void game::chat()
             .query();
             yell_msg = popup.text();
             is_order = false;
+            is_sentence_yell = true;
             break;
         }
         case NPC_CHAT_EMOTE: {
@@ -1497,6 +1499,20 @@ void game::chat()
     if( !message.empty() ) {
         add_msg( _( "You yell %s" ), message );
         u.shout( string_format( _( "%s yelling %s" ), u.disp_name(), message ), is_order );
+        if( is_sentence_yell && get_option<bool>( "DEBUG_LLM_INTENT" ) ) {
+            std::vector<npc *> hearers = get_npcs_if( [&]( const npc & guy ) {
+                return guy.can_hear( u.pos_bub(), volume );
+            } );
+            if( hearers.empty() ) {
+                add_msg( "LLM intent test: player yelled sentence %s (no NPCs heard it)", message );
+            } else {
+                std::string hearer_list = enumerate_as_string( hearers.begin(), hearers.end(),
+                []( const npc *guy ) {
+                    return guy->get_name();
+                } );
+                add_msg( "LLM intent test: player yelled sentence %s (heard by %s)", message, hearer_list );
+            }
+        }
     }
     if( !emote_msg.empty() ) {
         add_msg( emote_msg );
