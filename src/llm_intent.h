34152edeb65827c9a6e677b4b907236ed9e259fe
jo <josef.horvath@gmx.at>
2026-01-10 03:02:26 +0100

    working on phi. bit faster. now improving prompt and speed.

--- src/llm_intent.h
+++ src/llm_intent.h
@@ -6,6 +6,8 @@ class npc;
 
 namespace llm_intent
 {
+void enqueue_request( npc &listener, const std::string &player_utterance );
 void enqueue_request( const npc &listener, const std::string &player_utterance );
+void prewarm();
 void process_responses();
 } // namespace llm_intent
