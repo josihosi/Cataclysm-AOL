9b99c6961af411915d998746678152b51863da80
jo <josef.horvath@gmx.at>
2026-01-18 21:07:55 +0100

    Linux supported!(?)

--- just_build.cmd
+++ just_build.cmd
@@ -4,5 +4,9 @@ set "CLEAN=1"
 for %%A in (%*) do (
   if /I "%%~A"=="--unclean" set "CLEAN=0"
 )
+if exist config\options.windows.json (
+  echo Applying Windows LLM options snapshot...
+  copy /Y config\options.windows.json config\options.json >nul
+)
 
 "C:\Users\josef\dev\msys64\usr\bin\bash.exe" -lc "cd /c/Users/josef/dev/Cataclysm-AOL && if [ \"%CLEAN%\" = \"1\" ]; then echo Cleaning in MSYSTEM=$MSYSTEM; make clean; rm -f zstd.a; else echo Skipping clean in MSYSTEM=$MSYSTEM; fi && echo Building in MSYSTEM=$MSYSTEM && make -j$(nproc) RELEASE=1 MSYS2=1 DYNAMIC_LINKING=1 SDL=1 TILES=1 SOUND=1 LOCALIZE=0 LINTJSON=0 ASTYLE=0 TESTS=0"
